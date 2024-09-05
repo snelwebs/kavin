78c657a7d3ed55ce00b58972e56f544ad0c4c7a8
snelwebs <81949182+snelwebs@users.noreply.github.com>
2024-09-05 13:46:27 +0200

    Update Kavin.py

--- Kavin.py
+++ Kavin.py
@@ -1,11 +1,8 @@
 import streamlit as st
 
 st.write ("Hello World, this is my website where you can do nothing exept read the text you are reading wite now.")
+st.caption("Made from Kavin.D")
 st.caption("Made from Kavin.Dhanasekar")
 st.file_uploader("Upload A File")
 st.color_picker("")
 st.checkbox("")
-Admin_Check = st.chat_input("Enter Admin Code")
-st.code('print ("Hello world")')
-if Admin_Check == "redbell1234":
-    camera = False
