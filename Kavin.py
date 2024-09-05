9082b175154a9349ba29470eb0112b544c74a1d4
snelwebs <81949182+snelwebs@users.noreply.github.com>
2024-09-05 13:48:39 +0200

    Update Kavin.py

--- Kavin.py
+++ Kavin.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.write ("Hello World, this is my website where you can do nothing exept read the text you are reading wite now.")
-st.caption("Made from Kavin.D")
 st.caption("Made from Kavin.Dhanasekar")
 st.file_uploader("Upload A File")
 st.color_picker("")
 st.checkbox("")
+st.chat_message("For Palash and Antoni ONLY!")
